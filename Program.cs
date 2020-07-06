0f3bc65150d441f48e5efc94d4e74df3e1a01b3e
Mustafa Ali <mustafaalispk@yahoo.com>
2020-07-06 23:21:45 +0200

    Add register departure functionality with methods

--- Program.cs
+++ Program.cs
@@ -55,56 +55,14 @@ namespace Parkeringhus
 
                             DateTime arrival = DateTime.Now;
 
-                            // Vi sätter in @ symbol---> placeholder like  @Customer för att den kommer
-                            // vi skjuta in själva med värde och sen kommer skicka till servern. 
-                            string query = @"INSERT INTO Parking (Customer,
-                                                              ContactDetails,
-                                                              RegistrationNumber,
-                                                              Description,
-                                                              Arrival)
-                                       VALUES (@Customer,
-                                               @ContactDetails,
-                                               @RegistrationNumber,
-                                               @Description,
-                                               @Arrival)";
-                            // Vi behöver skappa upp ett objekt som representerar anslutning till databasen.
-                            // Det objektet känner till vad är connectionString som vi har skapat tidigare och
-                            // lägga in inne.
-
-                            SqlConnection connection = new SqlConnection(connectionString);
-
-                            // Nu har vi skappat nånting like connection som anslutar till server. Nu skappar vi
-                            // nånting som skickar sql commando till server. Det heter SqlCommand.
-                            // Då vill vi ha två grejer. 
-                            // 1. Det vill vi veta, vilken kommand ska vi köra och
-                            // 2. Vilken connection objekt och query string ska sätta in like query, connection?
-
-                            SqlCommand command = new SqlCommand(query, connection);
-
-                            // I nästa steg, vi behöver tilldela @Customer värde till variable customer och smma till andra variablar. Det gör vi genom command.
-
-                            command.Parameters.AddWithValue("@Customer", customer);
-
-                            command.Parameters.AddWithValue("@ContactDetails", contactDetails);
-
-                            command.Parameters.AddWithValue("@RegistrationNumber", registrationNumber);
-
-                            command.Parameters.AddWithValue("@Description", description);
-
-                            command.Parameters.AddWithValue("@Arrival", arrival);
-
-                            connection.Open();
-
-                            command.ExecuteNonQuery();
-
-                            connection.Close();
-
                             // Vi behöver koden som lägger in den Tabellen i Databasen
                             // TODO: Add method to insert record into database
 
+                            RegisterArrival(customer, contactDetails, registrationNumber, description, arrival);
+
                             Clear();
 
-                            WriteLine("Parking registered");
+                            WriteLine("Arrival registered");
 
                             Thread.Sleep(2000);
 
@@ -118,69 +76,38 @@ namespace Parkeringhus
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
 
-                        
+                        {
+
+                            Write("Registration number: ");
+
+                            string registrationNumber = ReadLine();
+
+                            DateTime departure = DateTime.Now;
+
+                            RegisterDeparture(registrationNumber, departure);
+                        }
+
+                        Clear();
+
+                        // TODO: Create notice snippet
+
+                        WriteLine("Departure registered");
+
+                        Thread.Sleep(2000);
+
+                        Clear();
 
                         break;
 
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
-                        { 
-                        WriteLine("ID  Registration Number     Arrival     Departure");
-                        WriteLine("-----------------------------------------------------");
+                        {
+                            ShowParkingRegistry();
 
-                            string query = @"SELECT Id
-                                              ,Customer
-                                              ,ContactDetails
-                                              ,RegistrationNumber
-                                              ,Description
-                                              ,Arrival
-                                              ,Departure
-                                              FROM Parking";
-
-                            // SqlConnection är det vi använder för att upprätta (establish) anslutning till servern och databasen hanteraren.  
-                            SqlConnection connection = new SqlConnection(connectionString);
-
-                            // SqlCommand är det ett Objekt som represeterar det kommandot vi köra. 
-                            SqlCommand command = new SqlCommand(query, connection);
-
-                            connection.Open();
-
-                            SqlDataReader dataReader = command.ExecuteReader();
-
-                            // den Read() inne i While loopen kommer att retunera en boolen --> true eller false
-                            // den  Read()--> försöker läsa en rad från databasen och om det gick och läsa, Om det fanns
-                            // en rad att läsa då retunera den true och då kan vi komma åt värden i dataReader.  
-                            while (dataReader.Read())
-                            {
-                                string id = dataReader["Id"].ToString();
-
-                                string registrationNumber = dataReader["RegistrationNumber"].ToString();
-
-                                string arrival = dataReader["Arrival"].ToString();
-
-                                string departure = dataReader["Departure"].ToString();
-
-                                Write(id.PadRight(5, ' '));
-                                Write(registrationNumber.PadRight(20, ' '));
-                                Write(arrival.PadRight(20, ' '));
-                                WriteLine(departure);
-
-                            }
-
-
-                            connection.Close();
-
-                            WriteLine();
-                            WriteLine("Press key to continue");
-
-                            ReadKey(true);
-
-                            Clear();
-
-                        break;
+                            break;
 
                         }
-                       
+
 
                     case ConsoleKey.D4:
                     case ConsoleKey.NumPad4:
@@ -190,6 +117,133 @@ namespace Parkeringhus
                         break;
                 }
             }
+        }
+
+        private static void ShowParkingRegistry()
+        {
+            WriteLine("ID  Registration Number     Arrival     Departure");
+            WriteLine("-----------------------------------------------------");
+
+            string query = @"SELECT Id
+                                              ,Customer
+                                              ,ContactDetails
+                                              ,RegistrationNumber
+                                              ,Description
+                                              ,Arrival
+                                              ,Departure
+                                              FROM Parking";
+
+            // SqlConnection är det vi använder för att upprätta (establish) anslutning till servern och databasen hanteraren.  
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // SqlCommand är det ett Objekt som represeterar det kommandot vi köra. 
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            // den Read() inne i While loopen kommer att retunera en boolen --> true eller false
+            // den  Read()--> försöker läsa en rad från databasen och om det gick och läsa, Om det fanns
+            // en rad att läsa då retunera den true och då kan vi komma åt värden i dataReader.  
+            while (dataReader.Read())
+            {
+                string id = dataReader["Id"].ToString();
+
+                string registrationNumber = dataReader["RegistrationNumber"].ToString();
+
+                string arrival = dataReader["Arrival"].ToString();
+
+                string departure = dataReader["Departure"].ToString();
+
+                Write(id.PadRight(5, ' '));
+                Write(registrationNumber.PadRight(24, ' '));
+                Write(arrival.PadRight(20, ' '));
+                WriteLine(departure);
+
+            }
+
+
+            connection.Close();
+
+            WriteLine();
+            WriteLine("<Press key to continue");
+
+            ReadKey(true);
+
+            Clear();
+        }
+
+        private static void RegisterDeparture(string registrationNumber, DateTime departure)
+        {
+            //  GETDATE() är samma som DateTime.Now.
+            string query = @"UPDATE Parking   
+                                           SET Departure = @Departure
+	                                       WHERE RegistrationNumber = @RegistrationNumber
+	                                       AND Departure IS NULL";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@RegistrationNumber", registrationNumber);
+
+            command.Parameters.AddWithValue("@Departure", departure);
+
+            connection.Open();
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        private static void RegisterArrival(string customer, string contactDetails, string registrationNumber, string description, DateTime arrival)
+        {
+            // Vi sätter in @ symbol---> placeholder like  @Customer för att den kommer
+            // vi skjuta in själva med värde och sen kommer skicka till servern. 
+            string query = @"INSERT INTO Parking (Customer,
+                                                              ContactDetails,
+                                                              RegistrationNumber,
+                                                              Description,
+                                                              Arrival)
+                                       VALUES (@Customer,
+                                               @ContactDetails,
+                                               @RegistrationNumber,
+                                               @Description,
+                                               @Arrival)";
+            // Vi behöver skappa upp ett objekt som representerar anslutning till databasen.
+            // Det objektet känner till vad är connectionString som vi har skapat tidigare och
+            // lägga in inne.
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // Nu har vi skappat nånting like connection som anslutar till server. Nu skappar vi
+            // nånting som skickar sql commando till server. Det heter SqlCommand.
+            // Då vill vi ha två grejer. 
+            // 1. Det vill vi veta, vilken kommand ska vi köra och
+            // 2. Vilken connection objekt och query string ska sätta in like query, connection?
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            // I nästa steg, vi behöver tilldela @Customer värde till variable customer och smma till andra variablar. Det gör vi genom command.
+
+            command.Parameters.AddWithValue("@Customer", customer);
+
+            command.Parameters.AddWithValue("@ContactDetails", contactDetails);
+
+            command.Parameters.AddWithValue("@RegistrationNumber", registrationNumber);
+
+            command.Parameters.AddWithValue("@Description", description);
+
+            command.Parameters.AddWithValue("@Arrival", arrival);
+
+            connection.Open();
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+ 
+            
         }
     }
 }
